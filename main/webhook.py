80f8f3779fda37466a49527c166d5fe252823f36
smadhavan-kiwi <46871946+sanalm@users.noreply.github.com>
2022-06-19 15:20:40 +0100

    remove ntp

--- main/webhook.py
+++ main/webhook.py
@@ -4,13 +4,11 @@ import ujson
 import machine
 import ubinascii
 import urandom as random
-import ntptime 
 import time
 from machine import RTC
 
 class Webhook:
     def __init__(self):
-        ntptime.settime()  # Synchronise the system time using NTP
         self.blueLed = machine.Pin(2, machine.Pin.OUT)
         self.ledInterval = 0.01
         self.readingInterval = 0.5
