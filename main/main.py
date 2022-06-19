79bb31b05d817d5c3840dfdf559261745597213b
smadhavan-kiwi <46871946+sanalm@users.noreply.github.com>
2022-06-19 13:48:50 +0100

    initial checkin

---
+++ main/main.py
@@ -0,0 +1,44 @@
+from main.config import Config
+from main.ota_updater import OTAUpdater
+from main.webhook import Webhook
+import os
+import machine
+
+def check_for_update_to_install_during_next_reboot():
+    o = OTAUpdater('https://github.com/sanalm/metropolis.git')
+    o.using_network(ssid, password)
+    o.check_for_update_to_install_during_next_reboot()
+
+def download_and_install_update_if_available():
+    o = OTAUpdater('https://github.com/sanalm/metropolis.git')
+    o.using_network(ssid, password)
+    o.download_and_install_update_if_available(ssid, password)
+
+def start():
+    # your custom code goes here. Something like this: ...
+    # from main.x import YourProject
+    # project = YourProject()
+    # ...
+    w = Webhook()
+    w.wait_twitter()
+
+def boot():
+    if 'next' in os.listdir():
+        download_and_install_update_if_available()
+    else:
+        check_for_update_to_install_during_next_reboot()
+    start()
+
+# load configuration for a file
+config = Config('main.conf')
+ssid = config.get('ssid')
+password = config.get('password')
+from_email = config.get('from_email')
+from_password = config.get('from_password')
+to_email = config.get('to_email')
+email_body = config.get('email_body')
+override_mail_from = config.get('override_mail_from')
+email_subject = config.get('email_subject')
+image_capture = config.get('image_capture')
+
+boot()
---
+++ main/webhook.py
@@ -0,0 +1,24 @@
+from time import sleep
+from machine import Pin
+import machine
+import ubinascii
+import urandom as random
+import ntptime 
+import time
+from machine import RTC
+
+class Webhook:
+    def __init__(self):
+        ntptime.settime()  # Synchronise the system time using NTP
+        self.blueLed = machine.Pin(2, machine.Pin.OUT)
+        self.ledInterval = 0.01
+        self.readingInterval = 0.5
+
+    def wait_wait_twitter(self):
+        # wait for webhook
+        self.blueLed.value(1)
+        sleep(self.ledInterval)
+        self.blueLed.value(0)
+        
+
+
